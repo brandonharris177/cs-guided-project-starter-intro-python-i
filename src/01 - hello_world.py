717de07e8173ad4049552e639ec1982119e6a2a1
Brandon-Harris <brandonharris177@gmail.com>
2020-06-01 15:58:36 -0600

    Day 1 done

--- src/01 - hello_world.py
+++ src/01 - hello_world.py
@@ -5,7 +5,7 @@ Hello, World!
 # print("This line will be printed.")
 
 # # 2. Indentation for blocks (four spaces)
-# x = 1
-# if x == 1: 
-#     # indented four spaces
-#     print("x is 1.")
+x = 1
+if x == 1: 
+    # indented four spaces
+    print("x is 1.")
